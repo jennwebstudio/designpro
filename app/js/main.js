28109565fd4192842b99e1cb4910f389645193f5
jennwebstudio <evgenia.ivanova_ru@yahoo.com>
2022-05-12 17:31:54 +0400

    add page content, sticky header, smooth scroll and animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,28 @@
 $(function () {
-  
+ 
+  wow = new WOW(
+  {
+    boxClass:     'wow',      // default
+    animateClass: 'animate__animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  );
+  wow.init();
+
+  $(".menu a, .logo, .intro__btn").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top - 120}, 1500);
+    });
+
+
+  $('.reviews__inner').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg"></button>',
+  });
+
+
 });
